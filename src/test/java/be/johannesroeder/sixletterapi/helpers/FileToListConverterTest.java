bdb58b65a58ccc48db9076273805dae1c2735666
JohannesRoeder <johannes.roeder@brightest.be>
2022-06-26 17:54:03 +0200

    Setting up test with temp file

--- src/test/java/be/johannesroeder/sixletterapi/helpers/FileToListConverterTest.java
+++ src/test/java/be/johannesroeder/sixletterapi/helpers/FileToListConverterTest.java
@@ -7,7 +7,9 @@ import org.junit.rules.TemporaryFolder;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.IIOException;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
@@ -22,13 +24,21 @@ public class FileToListConverterTest {
     public void setUpFile() {
         try {
             file = folder.newFile("testfile.txt");
+            writeToFile();
         } catch (IOException e) {
             System.err.println("An Error happened creating a temporary test file in " + this.getClass().getSimpleName());
         }
     }
 
+    private void writeToFile() throws IOException {
+        FileWriter writer = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(writer);
+        bufferedWriter.write("this\nis\na\nsimple\ntest\n\nto\nsee\nif\nthe\ntest\nworks");
+        bufferedWriter.close();
+    }
     @Test
     public void firstTest(){
-        List<String> list = FileToListConverter.convertToList(file);
+
+       // List<String> list = FileToListConverter.convertToList(file);
     }
 }
