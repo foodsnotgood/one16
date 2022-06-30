435fee292036e225fc92a672e93d530953fe8326
JohannesRoeder <johannes.roeder@brightest.be>
2022-06-30 08:39:03 +0200

    Change temp folder to MockMultipartFile from springframework. First test passed

--- src/main/java/be/johannesroeder/sixletterapi/helpers/FileToListConverter.java
+++ src/main/java/be/johannesroeder/sixletterapi/helpers/FileToListConverter.java
@@ -1,8 +1,32 @@
 package be.johannesroeder.sixletterapi.helpers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileToListConverter {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileToListConverter.class);
+    public static List<String> convertToList(MultipartFile file) {
+        List<String> list = new ArrayList<>();
+        try {
+            InputStream inputStream = file.getInputStream();
+            new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+                    .lines()
+                    .forEach(list::add);
+        } catch (IOException e) {
+            logger.error("Something went wrong while reading the file. Details: " + e.getMessage());
+        }
+        return list;
+    }
     //seperate all 6 letter words from non 6 letter words
 
 }
--- src/test/java/be/johannesroeder/sixletterapi/helpers/FileToListConverterTest.java
+++ src/test/java/be/johannesroeder/sixletterapi/helpers/FileToListConverterTest.java
@@ -1,46 +1,28 @@
 package be.johannesroeder.sixletterapi.helpers;
 
 
-
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FileToListConverterTest {
 
-    File file;
+    MockMultipartFile file;
 
-    @Rule
-    public TemporaryFolder folder = new TemporaryFolder();
-
-   @Before
-    public void setUpFile() {
-        try {
-            file = folder.newFile("testfile.txt");
-            writeToFile();
-        } catch (IOException e) {
-            System.err.println("An Error happened creating a temporary test file in " + this.getClass().getSimpleName());
-        }
-    }
-
-    private void writeToFile() throws IOException {
-        FileWriter writer = new FileWriter(file);
-        BufferedWriter bufferedWriter = new BufferedWriter(writer);
-        bufferedWriter.write("this\nis\na\nsimple\ntest\n\nto\nsee\nif\nthe\ntest\nworks");
-        bufferedWriter.close();
-    }
     @Test
     public void firstTest() {
-        System.out.println(file.length());
-        assertTrue(file.exists());
-       // List<String> list = FileToListConverter.convertToList(file);
+        file = new MockMultipartFile(
+                "file",
+                "mock.txt",
+                MediaType.TEXT_PLAIN_VALUE,
+                "testing\nthe\nconverter".getBytes()
+        );
+
+        List<String> convertedList = FileToListConverter.convertToList(file);
+        assertEquals(3, convertedList.size());
     }
 }
